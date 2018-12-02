3c656e6c4836b7096e81dd8a2a3c62a3e5ebb04d
JunNishimura <n.junjun0303@gmail.com>
2018-12-02 18:15:00 +0900

    update loading bar

--- Assets/Scripts/RadialProgressBar.cs
+++ Assets/Scripts/RadialProgressBar.cs
@@ -8,27 +8,62 @@ using UnityEngine.UI;
 
 public class RadialProgressBar : MonoBehaviour
 {
-    public Transform LoadingBar;
-    [SerializeField] private float currentAmount;
     [SerializeField] private float speed;
+    public GameObject LoadingBarObj;
+    public GameObject PauseButtonObj;
+    private Image loadingBar;
+    private Image loadingStateImage;
+    private Button pauseButton;
+    private Sprite pauseSprite;
+    private Sprite restartSprite;
+    private float currentAmount;
+    private bool loadingState;
 
     private void Awake()
     {
         currentAmount = 0;
+        loadingBar = LoadingBarObj.GetComponent<Image>();
+        loadingStateImage = PauseButtonObj.GetComponent<Image>();
+        pauseButton = PauseButtonObj.GetComponent<Button>();
+        pauseButton.onClick.AddListener(PauseButtonFunc);
+        pauseSprite = Resources.Load<Sprite>("pause");
+        restartSprite = Resources.Load<Sprite>("play");
+        loadingState = true; // if this is true, then let's increase currentAmount. if NOT, stop increasing.
+        loadingStateImage.sprite = pauseSprite; // as default, pauseSprite.
     }
 
     private void Update()
     {
-        if (currentAmount < 100)
+        // if loadingState is true, then increase the currentAmount
+        if (currentAmount < 100 && loadingState)
         {
             currentAmount += speed * Time.deltaTime;
         }
-        else
+        else if (currentAmount >= 100 && loadingState)
         {
             currentAmount = 0;
+        } 
+        else if (!loadingState)
+        {
+            currentAmount += 0;
         }
 
-        LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+        loadingBar.fillAmount = currentAmount / 100;
+    }
 
+    private void PauseButtonFunc()
+    {
+        // change the state
+        loadingState = !loadingState;
+        if (!loadingState)
+        {
+            // if the loading bar stops increasing, then show the restart image
+            loadingStateImage.sprite = restartSprite;
+        } 
+        else
+        {
+            // if the loading bar is increasing, then show the pause image
+            loadingStateImage.sprite = pauseSprite;
+        }
     }
 }
